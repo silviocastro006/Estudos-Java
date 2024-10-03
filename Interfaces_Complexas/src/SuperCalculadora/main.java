edc2a565e5435a8e85b7854f8f6aa2084eee8460
Sílvio Ricardo <silviocastro006@gmail.com>
2024-10-03 17:22:57 -0300

    Exercício 7 Curso em Video
    Calculadora usando interface e a biblioteca math

--- Interfaces_Complexas/src/SuperCalculadora/main.java
+++ Interfaces_Complexas/src/SuperCalculadora/main.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.math.*;
 
 public class main {
     public static void main(String[] args) {
@@ -20,7 +21,7 @@ public class main {
             tela.setSize(515,639); // Alterar o tamanho da tela principal
             tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             tela.setLocationRelativeTo(null); // Ao invés de usar o setbounds posso apenas indicar que a tela fica relativa, então como null, a mesma fica no mesmo
-
+            tela.setTitle("Exercício Curso em Vídeo aula 07");
 
             // Criação de uma imagem para inserir no label e colocar no fundo o
             ImageIcon label_fundo = new ImageIcon("src\\SuperCalculadora\\Imagens\\fundo_tela.png");
@@ -35,7 +36,7 @@ public class main {
             JLabel informe = new JLabel("Informe um valor:");
             informe.setFont(new Font("Calibri",Font.BOLD, 25));
             fundo.add(informe);
-            informe.setBounds(20, 120, 200, 50);
+            informe.setBounds(20, 150, 200, 50);
             informe.setForeground(Color.WHITE);
 
 
@@ -44,12 +45,81 @@ public class main {
             JSpinner spinner = new JSpinner();
             spinner.setFont(new Font("Arial", Font.BOLD, 20));
             fundo.add(spinner);
-            spinner.setBounds(220, 130, 125, 30);
-
-
-            // Definir a visualização das telas
-            tela.setResizable(false);
-            tela.setVisible(true);
+            spinner.setBounds(220, 158, 125, 30);
+            
+            
+            // Alterando a aparencia do tool tip
+            UIManager.put("ToolTip.background", Color.decode("#ffff8f"));
+            UIManager.put("ToolTip.foreground", Color.black);
+            UIManager.put("ToolTip.font", new Font("Artial", Font.BOLD, 20));
+            
+            // Labels para mostrar as informações do resultado
+            
+            // ------- Título
+            // Criação
+            JLabel div2 = new JLabel("Resto da divisão por 2");
+            JLabel ele_cubo = new JLabel("Elevado ao Cubo");
+            JLabel raiz_quad = new JLabel("Raiz Quadrada");
+            JLabel raiz_cub = new JLabel("Raiz Cúbica");
+            JLabel abs = new JLabel("Valor Absoluto");
+            
+            // Inserir no frame fundo
+            fundo.add(div2);
+            fundo.add(ele_cubo);
+            fundo.add(raiz_quad);
+            fundo.add(raiz_cub);
+            fundo.add(abs);
+            
+            // Alterar tamanho da fonte
+            div2.setFont(new Font("Arial", Font.BOLD, 15));
+            ele_cubo.setFont(new Font("Arial", Font.BOLD, 15));
+            raiz_quad.setFont(new Font("Arial", Font.BOLD, 15));
+            raiz_cub.setFont(new Font("Arial", Font.BOLD, 15));
+            abs.setFont(new Font("Arial", Font.BOLD, 15));
+            
+            // Organização da tela
+            div2.setBounds(30, 280, 160, 50);
+            ele_cubo.setBounds(30, 340, 160, 50);
+            raiz_quad.setBounds(30, 400, 160, 50);
+            raiz_cub.setBounds(30, 460, 160, 50);
+            abs.setBounds(30, 520, 160, 50);
+            
+            
+            // ------- Resultad
+            // Criação
+            JLabel res_div2 = new JLabel("0");
+            JLabel res_ele_cubo = new JLabel("0");
+            JLabel res_raiz_quad = new JLabel("0");
+            JLabel res_raiz_cub = new JLabel("0");
+            JLabel res_abs = new JLabel("0");
+            
+            // Inserir no frame fundo
+            fundo.add(res_div2);
+            fundo.add(res_ele_cubo);
+            fundo.add(res_raiz_quad);
+            fundo.add(res_raiz_cub);
+            fundo.add(res_abs);
+            
+            // Alterar tamanho da fonte
+            res_div2.setFont(new Font("Arial", Font.BOLD, 18));
+            res_ele_cubo.setFont(new Font("Arial", Font.BOLD, 18));
+            res_raiz_quad.setFont(new Font("Arial", Font.BOLD, 18));
+            res_raiz_cub.setFont(new Font("Arial", Font.BOLD, 18));
+            res_abs.setFont(new Font("Arial", Font.BOLD, 18));
+            
+            // Alterar cor da fonte
+            res_div2.setForeground(Color.decode("#51a30a"));
+            res_ele_cubo.setForeground(Color.decode("#51a30a"));
+            res_raiz_quad.setForeground(Color.decode("#51a30a"));
+            res_raiz_cub.setForeground(Color.decode("#51a30a"));
+            res_abs.setForeground(Color.decode("#51a30a"));
+            
+            // Organização da tela
+            res_div2.setBounds(215, 280, 160, 50);
+            res_ele_cubo.setBounds(170, 340, 160, 50);
+            res_raiz_quad.setBounds(170 , 400, 160, 50);
+            res_raiz_cub.setBounds(170 , 460, 160, 50);
+            res_abs.setBounds(170, 520, 160, 50);
 
 
             // Criar um botão
@@ -58,22 +128,67 @@ public class main {
             calcular.setBorderPainted(false);
             calcular.setContentAreaFilled(true);
             fundo.add(calcular);
-            calcular.setBounds(370, 100, 100, 100);
+            calcular.setBounds(370, 120, 100, 100);
+            calcular.setToolTipText("Clique para calcular!");
             
             // Adicionar um MouseListener para a animação
             calcular.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 // Diminuir o tamanho do botão para simular uma animação de clique
-                calcular.setBounds(371, 112, 100, 100);
+                calcular.setBounds(368, 118, 100, 100);                
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
                 // Reverter o tamanho do botão após um curto período
-                calcular.setBounds(370, 100, 100, 100);
+                calcular.setBounds(370, 120, 100, 100);
             }
         });
             
+            calcular.addActionListener(new ActionListener(){
+           
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    
+                    int num = Integer.parseInt(spinner.getValue().toString());
+                    if (num>100){
+                        JOptionPane.showMessageDialog(null, "Favor inserir um valor até 100!");
+                        spinner.setValue(0);
+                        res_div2.setText("0");
+                        res_ele_cubo.setText("0");
+                        res_raiz_quad.setText("0");
+                        res_raiz_cub.setText("0");
+                        res_abs.setText("0");      
+                        return;
+                        }
+                            
+
+                    // Resto da divisão
+                    int r = (int)num % 2;
+                    res_div2.setText(String.valueOf(r));
+
+                    // Elevado ao cubo
+                    float cub = (float) Math.pow(num, 3);
+                    res_ele_cubo.setText(String.format("%.2f",cub));
+
+                    // Raiz Quadrada
+                    float raiz = (float) Math.sqrt(num);
+                    res_raiz_quad.setText(String.format("%.2f",raiz));
+
+                    // Rais Cúbica
+                    float raiz_c = (float) Math.cbrt(num);
+                    res_raiz_cub.setText(String.format("%.2f",raiz_c));
+
+                    // Valor absoluto
+                    int abs = (int) Math.abs(num);
+                    res_abs.setText(String.valueOf(abs));
+                }
+            });
+            
+            
+            // Definir a visualização das telas
+            tela.setResizable(false);
+            tela.setVisible(true);
     }
 }
