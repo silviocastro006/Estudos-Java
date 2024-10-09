2d95b6e244c2ddaf9e77aab32f5482d6a3c33181
Sílvio Ricardo <silviocastro006@gmail.com>
2024-10-08 22:53:09 -0300

    Atualização java
    Atualização java

---
+++ Primeiros_Comandos/src/TesteFuncao/TesteFuncao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TesteFuncao;
+
+/**
+ *
+ * @author acer
+ */
+public class TesteFuncao {
+    public static void main(String[] args) {
+        System.out.println("Vai começar a contagem");
+        System.out.println(Operacoes.contador(1, 5));
+    }
+}
