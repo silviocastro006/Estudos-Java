35052132bde13e24fc735990c4e38a47bc034e08
Sílvio Ricardo <silviocastro006@gmail.com>
2024-10-20 16:38:41 -0300

    teste de classes

--- CursoJavaPOO/src/cursojavapoo/TesteClasses/Garrafa.java
+++ CursoJavaPOO/src/cursojavapoo/TesteClasses/Garrafa.java
@@ -1,13 +1,82 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cursojavapoo.TesteClasses;
 
-/**
- *
- * @author acer
- */
 public class Garrafa {
+    // Atributos
+    public String cor;
+    public int tamanho;
+    public String material;
+    private int estoque;
+    public String conteudo;
+    public int venda;
+
+    public Garrafa(String cor, int tamanho, String material, int estoque, String conteudo, int venda) {
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.material = material;
+        this.estoque = estoque;
+        this.conteudo = conteudo;
+        this.venda = venda;
+    }
+    
+    
+    public void status(){
+        System.out.println("===== Informações sobre a garrafa ===");
+        System.out.format("Cor: %s%n", this.cor);
+        System.out.format("Tamanho: ml %d%n", this.tamanho);
+        System.out.format("Material: %s%n", this.material);
+        System.out.format("Estoque: %d%n", this.estoque);
+        System.out.format("Conteudo: %s%n", this.conteudo);
+        System.out.format("Total de Vendas: %d%n", this.venda);
+        
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+    
+
+    
+    public void venda(int v){
+        this.venda +=1;
+        this.estoque -= v;
+    }
+    
+    
     
 }
--- CursoJavaPOO/src/cursojavapoo/TesteClasses/Main.java
+++ CursoJavaPOO/src/cursojavapoo/TesteClasses/Main.java
@@ -2,5 +2,31 @@ package cursojavapoo.TesteClasses;
 
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+        // Criando uma cadeira
+        Cadeira cad1 = new Cadeira();
+        
+        // Alterando os valores por métodos especiais
+        cad1.setTipo("madeira");
+        cad1.setMarca("Tok Stok");
+        cad1.SetValor(600.50);
+        cad1.setDefeito(true);
+        cad1.setEAN13(12346456);
+        
+        // Criando uma garrafa
+        Garrafa g1 = new Garrafa("Branca",400,"Plastico",3,"Suco",0);
+        Garrafa g2 = new Garrafa("Preta",500,"Aluminio",4,"Suco",1);
+        Garrafa g3 = new Garrafa("Transparente",700,"Vidro",50,"Cerveja",3);
+
+        // Realizar algumas vendas
+        g1.venda(2);
+        g2.venda(2);
+        g3.venda(20);
+        
+        // Printando os status
+        g1.status();
+        g2.status();
+        g3.status();
+    }
 }
